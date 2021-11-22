2026fd23f8a79dac4c084a53d3b2acc6f57d8c35
milind2916 <sainimilind84@gmail.com>
2021-11-23 00:13:17 +0530

    data initialisation

---
+++ backend/data/colleges.js
@@ -0,0 +1,103 @@
+const colleges= [
+    {
+        name:'California Institute of Technology',
+        image:'/images/1.jpg',
+        description: 
+        collegelink:'',
+        dataset:'caltechData.csv',
+
+    },
+
+     {
+        name:  'Harvard University',
+        image: '/images/2.jpg',
+        description: 
+        college link:'',
+        dataset:'harvardDataset.csv',
+
+    },
+    {
+        name:  'Washington University in St Louis',
+        image: '/images/3.jpg',
+        description: 
+        college link:'',
+        dataset:'washingtonUniversityData.csv',
+
+    },
+    {
+        name:  'Duke University',
+        image: '/images/4.jpg',
+        description: 
+        college link:'',
+        dataset:'dukeUniversityData.csv',
+
+    },
+    {
+        name:  'University of Chicago',
+        image: '/images/5.jpg',
+        description: 
+        college link:'',
+        dataset:'chicagoUniversityData.csv',
+
+    },
+     {
+        name:  'Stanford University',
+        image: '/images/6.jpg',
+        description: 
+        college link:'',
+        dataset:'stanfordUniversityData.csv',
+
+    },
+    {
+        name:  'Massachusetts Institute of Technology',
+        image: '/images/7.jpg',
+        description: 
+        college link:'',
+        dataset:'massachussetIntituteData.csv',
+
+    },
+     {
+        name:   'Yale University',
+        image: '/images/8.jpg',
+        description: 
+        college link:'',
+        dataset:'yaleUniversityData.csv',
+        
+
+    },
+    {
+        name:   'Princeton University',
+        image: '/images/9.jpg',
+        description: 
+        college link:'',
+        dataset:'princetonUniversityData.csv',
+
+    },
+     {
+        name:   'Cornell University',
+        image: '/images/10.jpg',
+        description: 
+        college link:'',
+        dataset:'cornellUniversityData.csv',
+
+    },
+     {
+        name:    'Johns Hopkins University',
+        image: '/images/11.jpg',
+        description: 
+        college link:'',
+        dataset:'johnsHopkinsData.csv',
+
+    },
+    {
+        name:     'Rice University',
+        image: '/images/12.jpg',
+        description: 
+        college link:'',
+        dataset:'riceUniversityData.csv',
+
+    },
+    
+
+]
+export default colleges
